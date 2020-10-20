a0f9e82eddcb4efb5887ddfe880e1fc2db05fc85
Gianluca Andreis <gianluca.andreis@gmail.com>
2020-10-20 20:01:40 +0200

    plotter

---
+++ site/scripts/plotter.js
@@ -0,0 +1,14 @@
+function Plotter(){
+    self = this;
+
+
+    self.init = function(){
+
+        let html = 
+            '<div class="plotter-container">' +
+                
+            '</div>';
+            
+
+    }
+}
