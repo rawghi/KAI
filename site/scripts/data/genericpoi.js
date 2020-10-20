3ffa816f81bb727875be5c03bd56636c5032514a
Gianluca Andreis <gianluca.andreis@gmail.com>
2020-10-20 11:33:10 +0200

    Added map locations

--- site/scripts/data/genericpoi.js
+++ site/scripts/data/genericpoi.js
@@ -3,66 +3,77 @@ const data_genericpoi = [
         "name": "Woomerang Launch Site",
         "coords": [45.29, 136.11],
         "icon": "launchsite",
-        "description": "Woomerang Launch Site"
+        "description": "Woomerang Launch Site",
+        "image": "location_woomerang.png"
     },
     {
         "name": "Dessert Launch Site",
         "coords": [-6.560278, -143.95],
         "icon": "launchsite",
-        "description": "Dessert Launch Site"
+        "description": "Dessert Launch Site",
+        "image": "location_dessertlaunchpad.png"
     },
     {
         "name": "Dessert Airfield",
         "coords": [-8.599444, -144.040556],
         "icon": "airfield",
-        "description": "Dessert Airfield"
+        "description": "Dessert Airfield",
+        "image": "location_dessertairfield.png"
     },
     {
         "name": "Island Airfield",
         "coords": [-1.540833, -71.909722],
         "icon": "airfield",
-        "description": "Island Airfield"
+        "description": "Island Airfield",
+        "image": "location_island.png"
     },
     {
         "name": "Baikerbanur",
         "coords": [20.657222, -146.420556],
         "icon": "launchsite",
-        "description": "Baikerbanur"
+        "description": "Baikerbanur",
+        "image": "location_ksc2.png"
     },
     {
         "name": "Crater Rim",
         "coords": [9.45, -172.110278],
         "icon": "tracking",
-        "description": "Crater Rim"
+        "description": "Crater Rim",
+        "image": "location_dsn.jpeg"
     },
     {
         "name": "Nye Island",
         "coords": [5.363611, 108.548056],
         "icon": "tracking",
-        "description": "Nye Island"
+        "description": "Nye Island",
+        "image": "location_dsn.jpeg"
     },
     {
         "name": "Harvester Massif",
         "coords": [-11.95, 33.740278],
         "icon": "tracking",
-        "description": "Harvester Massif"
+        "description": "Harvester Massif",
+        "image": "location_dsn.jpeg"
     },
     {
         "name": "North Station One",
         "coords": [63.095, -90.079722],
         "icon": "tracking",
-        "description": "North Station One"
+        "description": "North Station One",
+        "image": "location_dsn.jpeg"
     },
     {
         "name": "Mesa South",
         "coords": [-59.589722, -25.861667],
         "icon": "tracking",
-        "description": "Mesa South"
+        "description": "Mesa South",
+        "image": "location_dsn.jpeg"
     },
     {
         "name": "Kerbal Space Center",
         "coords": [-0.1025, -74.575278],
         "icon": "vab",
-        "description": "Kerbal Space Center"
+        "description": "Kerbal Space Center",
+        "image": "location_ksc.png"
     }
 ];
